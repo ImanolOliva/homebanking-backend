e6ddc995eedc823ef8c9d409b1e577bb898bac35
ImanolOliva <imanoleduardooliva@gmail.com>
2023-10-02 20:55:21 -0300

    frontend y backend funcionando

--- src/main/java/com/example/security/demo/DemoApplication.java
+++ src/main/java/com/example/security/demo/DemoApplication.java
@@ -19,7 +19,7 @@ public class DemoApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://chic-salmiakki-3e384e.netlify.app/").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("https://challenge-frontendforb.netlify.app/").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
